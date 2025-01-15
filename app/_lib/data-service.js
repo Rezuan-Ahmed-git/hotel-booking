6df0c1cdd071bd19a2bcae8f756a16fbef55347e
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-01-15 22:35:39 +0600

    feat: create new reservation functionality implement

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -163,7 +163,7 @@ export async function createGuest(newGuest) {
   return data;
 }
 
-export async function createBooking(newBooking) {
+/* export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from('bookings')
     .insert([newBooking])
@@ -177,7 +177,7 @@ export async function createBooking(newBooking) {
   }
 
   return data;
-}
+} */
 
 /////////////
 // UPDATE
