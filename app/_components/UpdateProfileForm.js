3e80166cb263d7b38d21ea5612f9f04f673c7372
Rezuan-Ahmed-git <rezuanahmed.dev@gmail.com>
2025-01-15 18:23:59 +0600

    feat: updating reservation functionality implement

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,7 +1,7 @@
 'use client';
-import { useFormStatus } from 'react-dom';
 import { useState } from 'react';
 import { updateGuest } from '../_lib/actions';
+import SubmitButton from './SubmitButton';
 
 function UpdateProfileForm({ children, guest }) {
   const [count, setCount] = useState();
@@ -56,22 +56,9 @@ function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update Profile</SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? 'Updating...' : ' Update profile'}
-    </button>
   );
 }
 
